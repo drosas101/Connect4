2a66e5358a22af1c7b1c64cb30fcb4efd3a3cf3c
David Rosas <150191617+drosas101@users.noreply.github.com>
2023-11-09 16:00:37 -0800

    Update Player.java
    added playerColor and playerName
    added set and get methods
    added myTurn method to return player selection

--- Player.java
+++ Player.java
@@ -1,21 +1,48 @@
 package connect4;
-
+import java.util.Scanner;
 /**
  *
  * @author David Rosas
  */
 public class Player {
     private int playerNumber;
-    
-    public Player(int playerNumber){
+    private String playerColor;
+    private String playerName;
+        
+    public Player(int playerNumber,String playerColor){
         this.playerNumber = playerNumber;
+        this.playerColor = playerColor;
+        this.playerName = "Player" + " " + playerNumber;
     }
     
+    public void setPlayerName(int playerNumber){
+        this.playerName = "Player" + " " + playerNumber;
+    }
+    
+    public String getPlayerName(){
+        return this.playerName;
+    }
+   
     public void setPlayerNumber(int playerNumber){
         this.playerNumber = playerNumber;
     }
     
     public int getPlayerNumber(){
         return this.playerNumber;
+    }
+    
+    public void setPlayerColor(String color){
+        this.playerColor = color;
+    }
+    
+    public String getPlayerColor(){
+        return this.playerColor;
+    }
+    
+    public int myTurn(){        
+        System.out.printf("%s's turn.\n",this.playerName);
+        System.out.print("Enter a column: ");
+        Scanner input = new Scanner(System.in);
+        return input.nextInt();
     }
 }
