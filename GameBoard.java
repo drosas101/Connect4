657db2434532d9aebfa6d2593953c7eebee9d0ea
David Rosas <150191617+drosas101@users.noreply.github.com>
2023-11-14 20:35:52 -0800

    Update GameBoard.java
    added playerTurn( ) method
    
    added get and set methods for all variables
    
    added isGameOver( ) to check is game is over
    
    added checkForWin( ) to check for a winning move
    
    added isBoardFull( ) to check if the board is full

--- GameBoard.java
+++ GameBoard.java
@@ -1,5 +1,7 @@
 package connect4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author David Rosas
@@ -8,8 +10,33 @@ public class GameBoard {
 
     private int rows = 6;
     private int columns = 7;
+    private boolean gameOver = false;
+    private Piece[][] mainBoard = new Piece[rows][columns];
 
-    Piece[][] mainBoard = new Piece[rows][columns];
+    //get and set methods
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    public boolean getGameState() {
+        return this.gameOver;
+    }
+
+    public void setGameState(boolean state) {
+        this.gameOver = state;
+    }
 
     //GameBoard constructor sets all values of the array to null
     public GameBoard() {
@@ -25,6 +52,8 @@ public class GameBoard {
     public void placePiece(int column, String color) {
         int col = column - 1;
 
+        //since we already check for a full column in playerTurn()
+        //we might be able to remove the if statement
         if (mainBoard[0][col] == null) {
             for (int i = rows - 1; i >= 0; i--) {
                 if (mainBoard[i][col] == null) {
@@ -33,8 +62,6 @@ public class GameBoard {
                     break;
                 }
             }
-        } else{
-            System.out.print("this column is full");
         }
     }
 
@@ -53,6 +80,47 @@ public class GameBoard {
             }
             System.out.println();
         }
+    }
+
+    //player enter's column for their piece here. we check if the column is full
+    //if the column is full then we ask for a different column. repeat until
+    //we find a suitable column to play to. 
+    public void playerTurn(String playerName, String playerColor) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("%s's turn.\n", playerName);
+        System.out.print("Enter a column: ");
+        int col = input.nextInt();
+        boolean tryAgain = isColumnFull(col);
+
+        while (!tryAgain) {
+            System.out.printf("Column full. Enter a new column; ");
+            col = input.nextInt();
+            tryAgain = isColumnFull(col);
+        }
+
+        placePiece(col, playerColor);
+
+    }
+
+    //check if a column is full
+    public boolean isColumnFull(int inCol) {
+        return mainBoard[0][(inCol - 1)] == null;
+    }
+
+    //return the current gamestate to see if the game is over
+    public boolean isGameOver() {
+        return getGameState();
+    }
+
+    public boolean checkForWin(String color) {
+        // need logic to check for a win
+        return false;
+    }
+
+    public boolean isBoardFull() {
+        // need logic to check for win
+        return false;
     }
 
 }
